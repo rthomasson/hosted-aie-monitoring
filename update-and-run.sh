bb41afbbf8b159135d9f87640021d89c26a89c11
Randy Thomasson <randy.thomasson@gmail.com>
2025-07-11 15:15:29 -0700

    Update update-and-run.sh
    Modified as part of the changes to add support for sending status metrics to Grafana Cloud

--- update-and-run.sh
+++ update-and-run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 curl https://raw.githubusercontent.com/abe-hpe/hosted-aie-monitoring/refs/heads/main/aiemon.sh > ~/.aiemon/aiemon.sh
+curl https://raw.githubusercontent.com/abe-hpe/hosted-aie-monitoring/refs/heads/main/aiemon.sh > ~/.aiemon/aiemon_grafana.sh
 curl https://raw.githubusercontent.com/abe-hpe/hosted-aie-monitoring/refs/heads/main/mail.json > ~/.aiemon/mail.json
 curl https://raw.githubusercontent.com/abe-hpe/hosted-aie-monitoring/refs/heads/main/slack.json > ~/.aiemon/slack.json
+curl https://raw.githubusercontent.com/abe-hpe/hosted-aie-monitoring/refs/heads/main/slack.json > ~/.aiemon/graphite.json
+chmod a+x ~/.aiemon/aiemon_grafana.sh
 chmod a+x ~/.aiemon/aiemon.sh
 ~/.aiemon/aiemon.sh
